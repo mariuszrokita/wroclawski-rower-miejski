302e3571952e032951d0d12847539bd5a7d4bf71
Mariusz Rokita <mariusz.rokita@gmail.com>
2019-08-31 13:27:54 +0200

    Created module for incremental data import (script needs to be run manually).

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # wroclawski-rower-miejski
+
+## Set up virtual environment
+
+```bash
+conda create --name nextbike python=3.6.8
+
+conda activate nextbike
+```
+
+## Install dependencies
+
+```bash
+pip install -r requirements.txt
+```
---
+++ src/data/incremental_data_import.py
@@ -0,0 +1,52 @@
+"""
+This module contains functions to import csv files with historic records 
+and to persist them in Azure Blob Storage.
+"""
+
+import os
+import requests
+
+from azure.storage.blob import BlockBlobService
+from bs4 import BeautifulSoup
+
+
+def get_historic_csv_file_links_to_download(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, features="html.parser")
+
+    csv_anchors = soup.find_all(name="a", class_="resource-url-analytics")
+    return [ link['href'] 
+                for link in csv_anchors
+                if "Pobierz" in link.text ]
+
+
+def get_blob_names_already_imported(block_blob_service, container_name):
+    generator = block_blob_service.list_blobs(container_name)
+    return [blob.name for blob in generator]
+
+
+def import_historic_csv_files(url, account_name, account_key, container_name):
+    block_blob_service = BlockBlobService(account_name, account_key)
+    blobs_already_downloaded = get_blob_names_already_imported(block_blob_service, container_name)
+
+    historic_csv_links = get_historic_csv_file_links_to_download(url)
+    for csv_link in historic_csv_links:
+        filename = os.path.basename(csv_link)
+        if not filename in blobs_already_downloaded:
+            # read csv file content and create blob from it
+            csv = requests.get(csv_link)
+            block_blob_service.create_blob_from_text(storage_container_name, filename, csv.text)
+
+
+if __name__ == "__main__":
+    # configuration
+    historic_records_url = 'https://www.wroclaw.pl/open-data/dataset/wrmprzejazdy_data/resource_history/65b5015e-070e-41da-8852-802a86442ac5'
+    storage_account_name = ''
+    storage_account_key = ''
+    storage_container_name = ''
+
+    # run import
+    import_historic_csv_files(historic_records_url, 
+                            storage_account_name, 
+                            storage_account_key, 
+                            storage_container_name)
