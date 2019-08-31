79b58dd27da27ca47e1e06ce0212119f0861094b
Mariusz Rokita <mariusz.rokita@gmail.com>
2019-08-31 18:47:23 +0200

    Subtle refactoring.

--- src/data/incremental_data_import.py
+++ src/data/incremental_data_import.py
@@ -27,13 +27,13 @@ def get_blob_names_already_imported(block_blob_service, container_name):
 
 def import_historic_csv_files(url, account_name, account_key, container_name):
     block_blob_service = BlockBlobService(account_name, account_key)
-    blobs_already_downloaded = get_blob_names_already_imported(block_blob_service, container_name)
+    blobs_already_imported = get_blob_names_already_imported(block_blob_service, container_name)
 
     historic_csv_links = get_historic_csv_file_links_to_download(url)
     for csv_link in historic_csv_links:
         filename = os.path.basename(csv_link)
-        if not filename in blobs_already_downloaded:
-            # read csv file content and create blob from it
+        if not filename in blobs_already_imported:
+            # import csv file - read file's content and upload it to Azure Blob Storage
             csv = requests.get(csv_link)
             block_blob_service.create_blob_from_text(storage_container_name, filename, csv.text)
 
