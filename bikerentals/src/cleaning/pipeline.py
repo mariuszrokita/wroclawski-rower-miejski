b6ac3dce096f104598ea6abd186b5038435c4f9f
Mariusz Rokita <mariusz.rokita@gmail.com>
2019-10-31 07:47:36 +0100

    Execute pipeline with or without removing data records.

--- bikerentals/src/cleaning/pipeline.py
+++ bikerentals/src/cleaning/pipeline.py
@@ -2,11 +2,24 @@ from sklearn.pipeline import make_pipeline
 
 from bikerentals.src.cleaning.remove_same_location import SameLocationRemover
 
-def execute(df):
-    
-    data_cleaning_pipeline = make_pipeline(
-        SameLocationRemover('Rental station', 'Return station')
-    )
+
+def execute(df, remove_records):
+    """
+    Execute data cleaning pipeline.
+
+    Parameters:
+    * df - dataframe
+    * remove_records - indicate whether to remove some records while cleaning
+                       data or not.
+    """
+
+    if remove_records:
+        data_cleaning_pipeline = make_pipeline(
+            SameLocationRemover('Rental station', 'Return station')
+        )
+    else:
+        # there's no work defined, just return
+        return df
 
     # execute pipeline and return result
     processed_df = data_cleaning_pipeline.transform(df)
