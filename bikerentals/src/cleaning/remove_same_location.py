2342a04be76076240c0130d622e00b7fcaf42ad2
Mariusz Rokita <mariusz.rokita@gmail.com>
2019-10-31 12:50:39 +0100

    Created unit tests.

--- bikerentals/src/cleaning/remove_same_location.py
+++ bikerentals/src/cleaning/remove_same_location.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -8,21 +7,23 @@ class SameLocationRemover(BaseEstimator, TransformerMixin):
     Removes records that were rented from and returned to the same bike station.
     """
 
-    def __init__(self, rental_station_col, return_station_col):
+    def __init__(self, rental_station_col: str, return_station_col: str):
         self.rental_station_col = rental_station_col
         self.return_station_col = return_station_col
 
-    def fit(self, X, y=None):
+    def fit(self, X: pd.DataFrame, y=None) -> pd.DataFrame:
         return self
 
-    def transform(self, X):
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         assert isinstance(X, pd.DataFrame)
-        
+
         print("* SameLocationRemover *")
         print("--> input data shape: ", X.shape)
 
-        # TODO: write logic
+        # Delete all records for which bike is returned to the same bike station,
+        # as such situation is not considered as a regular commute.
+        idx = X[X[self.rental_station_col] == X[self.return_station_col]].index
+        X = X.drop(index=idx, axis=0)
 
         print("--> output data shape: ", X.shape)
         return X
-  
--- bikerentals/src/tests/cleaning/test_remove_same_location.py
+++ bikerentals/src/tests/cleaning/test_remove_same_location.py
@@ -1,11 +1,64 @@
-import numpy as np
 import pandas as pd
 import pytest
-
 from pandas.util.testing import assert_frame_equal
 
 from bikerentals.src.cleaning.remove_same_location import SameLocationRemover
 
 
 class TestSameLocationRemover:
-    pass
+
+    def test_should_remove_records_when_rental_station_and_return_station_is_same(self):
+        # arrange
+        data = {
+            'UID': [1, 2, 3, 4, 5],
+            'Rental station': ['Station A', 'Station B', 'Station C', 'Station D', 'Station E'],
+            'Return station': ['Station Z', 'Station B', 'Station D', 'Station D', 'Station B'],
+            'Duration': [2, 2, 1, 4, 5]
+        }
+
+        expected = {
+            'UID': [1, 3, 5],
+            'Rental station': ['Station A', 'Station C', 'Station E'],
+            'Return station': ['Station Z', 'Station D', 'Station B'],
+            'Duration': [2, 1, 5]
+        }
+
+        df = pd.DataFrame(data).set_index('UID')
+        expected_df = pd.DataFrame(expected).set_index('UID')
+
+        # act
+        sut = SameLocationRemover('Rental station', 'Return station')
+        actual_df = sut.transform(df)
+
+        # assert
+        assert_frame_equal(actual_df, expected_df)
+
+    def test_should_throw_when_missing_rental_station_column(self):
+        # arrange
+        data = {
+            'Rental station': [],
+            'Return station': []
+        }
+
+        df = pd.DataFrame(data)
+
+        # act
+        sut = SameLocationRemover('some col name', 'Return station')
+
+        with pytest.raises(KeyError, match='some col name'):
+            sut.transform(df)
+
+    def test_should_throw_when_missing_return_station_column(self):
+        # arrange
+        data = {
+            'Rental station': [],
+            'Return station': []
+        }
+
+        df = pd.DataFrame(data)
+
+        # act
+        sut = SameLocationRemover('Rental station', 'some col name')
+
+        with pytest.raises(KeyError, match='some col name'):
+            sut.transform(df)
