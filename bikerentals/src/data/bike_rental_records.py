e8a9d7f3b2ead147bcaef74bb23f0a8a7a8f6d3f
Mariusz Rokita <mariusz.rokita@gmail.com>
2019-11-01 23:21:59 +0100

    Fixing problems with incorrect data.

--- bikerentals/src/data/bike_rental_records.py
+++ bikerentals/src/data/bike_rental_records.py
@@ -28,12 +28,27 @@ class BikeRentalRecords:
     def __clean(self, df):
         # Last record is considered as unique and rest of the same values as duplicate
         df = df.drop_duplicates(subset="UID wynajmu", keep='last')
+
         # Sort it
         df = df.sort_values(by='UID wynajmu')
 
         # Make it convenient for the English-speaking audience
         df.columns = ['UID', 'Bike number', 'Rental datetime', 'Return datetime',
                       'Rental station', 'Return station', 'Duration']
+
+        # Remove whitespace characters
+        df.replace(u'\xa0$', u'', regex=True, inplace=True)
+
+        # Remove leading and trailing whitespace characters
+        df['Rental station'] = df['Rental station'].str.strip()
+        df['Return station'] = df['Return station'].str.strip()
+
+        # Fix wordings
+        for col in ['Rental station', 'Return station']:
+            df[col] = df[col].str.replace('dworzec główny', 'Dworzec Główny')
+            df[col] = df[col].str.replace('Dworzec Główny, północ', 'Dworzec Główny')
+            df[col] = df[col].str.replace('Drobnera - Dubois', 'Drobnera / Dubois')
+            df[col] = df[col].str.replace('Świdnicka - Piłsudskiego', 'Świdnicka / Piłsudskiego (Hotel Scandic)')
 
         return df
 
