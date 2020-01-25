a30c3906edfd260c5f92dc1d44633519c074d037
Mariusz Rokita <mariusz.rokita@gmail.com>
2020-01-25 22:49:10 +0100

    Writing data to a folder with intermediate data (json files converted to one csv file).

--- bikeavailability/aml/aml_setup.py
+++ bikeavailability/aml/aml_setup.py
@@ -1,7 +1,7 @@
 import azureml.core
 import os
 
-from azureml.core import Workspace, Datastore
+from azureml.core import Datastore, Experiment, Workspace
 from azureml.core.compute import ComputeTarget, AmlCompute
 from azureml.data.data_reference import DataReference
 from azureml.pipeline.core import Pipeline, PipelineData
@@ -36,9 +36,9 @@ def create_workspace(subscription_id, resource_group, region, workspace_name):
     return ws
 
 
-def create_computetarget(workspace):
+def create_compute_target(workspace):
     compute_name = "aml-compute"
-    vm_size = "STANDARD_D2_V2"
+    vm_size = "Standard_D1_v2"
 
     compute_target = None
     if compute_name in ws.compute_targets:
@@ -50,7 +50,7 @@ def create_computetarget(workspace):
         provisioning_config = AmlCompute.provisioning_configuration(vm_size=vm_size,
                                                                     min_nodes=0,
                                                                     max_nodes=4,
-                                                                    idle_seconds_before_scaledown=600)
+                                                                    idle_seconds_before_scaledown=900)
         # create the compute target
         compute_target = ComputeTarget.create(ws, compute_name, provisioning_config)
 
@@ -59,12 +59,12 @@ def create_computetarget(workspace):
         compute_target.wait_for_completion(show_output=True, min_node_count=None, timeout_in_minutes=20)
 
         # For a more detailed view of current cluster status, use the 'status' property
-        #print(compute_target.status.serialize())
+        # print(compute_target.status.serialize())
         print("The compute target created!")
     return compute_target
 
 
-def set_up_datastores(workspace, account_name, account_key, container_name):
+def set_up_input_datastore(workspace, account_name, account_key, container_name):
     # List all datastores registered in the current workspace
     datastores = [datastore for datastore in workspace.datastores]
 
@@ -75,17 +75,54 @@ def set_up_datastores(workspace, account_name, account_key, container_name):
         input_datastore = Datastore.register_azure_blob_container(
             workspace=workspace,
             datastore_name=input_datastore_name,
-            account_name=account_name,  # Storage account name
-            container_name=container_name,  # Name of Azure blob container
-            account_key=account_key)  # Storage account key
+            account_name=account_name,
+            container_name=container_name,
+            account_key=account_key)
         print(f"Registered input blob datastore with name: {input_datastore_name}")
     else:
         input_datastore = Datastore.get(workspace, input_datastore_name)
         print(f"No need to register input blob datastore with name: {input_datastore_name}. It's already registered!")
+    return input_datastore
 
-    # default datastore to persist interim and processed data
-    output_datastore = ws.get_default_datastore()
-    return input_datastore, output_datastore
+
+def set_up_output_datastore(workspace, account_name, account_key, container_name):
+    # List all datastores registered in the current workspace
+    datastores = [datastore for datastore in workspace.datastores]
+
+    output_datastore = None
+    output_datastore_name = 'outputblobstorage'
+    if len(datastores) == 0 or output_datastore_name not in datastores:
+        print("Registering output blob datastore...")
+        output_datastore = Datastore.register_azure_blob_container(
+            workspace=workspace,
+            datastore_name=output_datastore_name,
+            account_name=account_name,
+            container_name=container_name,
+            account_key=account_key)
+        print(f"Registered output blob datastore with name: {output_datastore_name}")
+    else:
+        output_datastore = Datastore.get(workspace, output_datastore_name)
+        print(f"No need to register input blob datastore with name: {output_datastore_name}. It's already registered!")
+    return output_datastore
+
+
+def create_step_ingest_and_convert(input_data_loc, intermediate_data_loc, output_data, compute_target):
+    source_directory = 'ingestion'
+    print(f"Source directory for the step is {os.path.realpath(source_directory)}.")
+    # TODO: The converted csv file should be copied to a known 'fix' location.
+    step = PythonScriptStep(
+        name="Ingest json files and convert to csv",
+        script_name="ingest.py",
+        arguments=["--input", input_data_loc,
+                   "--output", output_data,
+                   "--converted_data_location", intermediate_data_loc],
+        inputs=[input_data_loc, intermediate_data_loc],
+        outputs=[output_data],
+        compute_target=compute_target,
+        source_directory=source_directory,
+        allow_reuse=True)
+    print("Step created!")
+    return step
 
 
 if __name__ == "__main__":
@@ -95,9 +132,12 @@ if __name__ == "__main__":
     resource_group = os.getenv("RESOURCE_GROUP")
     region = os.getenv("REGION")
     workspace_name = os.getenv("WORKSPACE_NAME")
-    storage_account_name = os.getenv("STORAGE_ACCOUNT")
-    storage_account_key = os.getenv("STORAGE_KEY")
-    container_name = os.getenv("CONTAINER_NAME")
+    input_storage_account_name = os.getenv("INPUT_STORAGE_ACCOUNT")
+    input_storage_account_key = os.getenv("INPUT_STORAGE_KEY")
+    input_container_name = os.getenv("INPUT_CONTAINER_NAME")
+    output_storage_account_name = os.getenv("OUTPUT_STORAGE_ACCOUNT")
+    output_storage_account_key = os.getenv("OUTPUT_STORAGE_KEY")
+    output_container_name = os.getenv("OUTPUT_CONTAINER_NAME")
 
     # Create an Azure Machine Learning service workspace
     print("\nSTEP 1")
@@ -105,33 +145,68 @@ if __name__ == "__main__":
 
     # Create a computer target
     print("\nSTEP 2")
-    compute_target = create_computetarget(ws)
+    compute_target = create_compute_target(ws)
 
-    # Create references to source/input and destination/output datasources
+    # Create source/input datasource that contains raw json files
     print("\nSTEP 3")
-    input_datastore, output_datastore = set_up_datastores(ws, storage_account_name, storage_account_key, container_name)
+    input_datastore = set_up_input_datastore(
+        workspace=ws,
+        account_name=input_storage_account_name,
+        account_key=input_storage_account_key,
+        container_name=input_container_name)
 
-    input_data = DataReference(
+    print("Creating reference to raw input data...")
+    input_data_ref = DataReference(
         datastore=input_datastore,
-        data_reference_name="input_data"
-    )
+        data_reference_name="raw_data")
+    print("done!")
 
-    processed_data1 = PipelineData("processed_data1", datastore=output_datastore)
+    # Datastore that should store data available to all pipeline steps
+    print("\nSTEP 4")
+    output_datastore = set_up_output_datastore(
+        workspace=ws,
+        account_name=output_storage_account_name,
+        account_key=output_storage_account_key,
+        container_name=output_container_name)
 
-    source_directory = 'ingestion'
-    print(f"Source directory for the step is {os.path.realpath(source_directory)}.")
-    ingestStep = PythonScriptStep(
-        script_name="ingest.py",
-        arguments=["--input", input_data, "--output", processed_data1],
-        inputs=[input_data],
-        outputs=[processed_data1],
-        compute_target=compute_target,
-        source_directory=source_directory,
-        allow_reuse=True)
+    converted_data_ref = DataReference(
+        datastore=output_datastore,
+        data_reference_name="converted_data")
 
-    pipeline_steps = [ingestStep]
-    pipeline1 = Pipeline(workspace=ws, steps=[pipeline_steps])
+    # Default datastore to exchange data between pipeline steps
+    print("\nSTEP 5")
+    print("Creating datastore used to exchange data between pipeline steps")
+    pipeline_datastore = ws.get_default_datastore()
 
-    published_pipeline1 = pipeline1.publish(name="Data Preparation Pipeline",
-                                            description="Data Preparation PipelineDescription",
-                                            continue_on_step_failure=True)
+    print("Creating pipeline data object (for data exchanged between pipeline steps)")
+    pipeline_data = PipelineData("pipeline_data", datastore=pipeline_datastore)
+
+    # Create pipeline steps
+    print("\nSTEP 6")
+    ingest_step = create_step_ingest_and_convert(
+        input_data_loc=input_data_ref,
+        intermediate_data_loc=converted_data_ref,
+        output_data=pipeline_data,
+        compute_target=compute_target)
+
+    # finally, create pipeline and publish it
+    # TODO: Is there any way to 'update' existing pipeline, instead of creating a new one?
+    print("\nSTEP 7")
+    print("Creating pipeline...")
+    pipeline_steps = [ingest_step]
+    pipeline = Pipeline(workspace=ws, steps=[pipeline_steps])
+    print("done!")
+
+    # TODO: Let the data preparation pipeline be executed in the scheduled manner
+    print("\nSTEP 8")
+    print("Publishing pipeline...")
+    published_pipeline = pipeline.publish(
+        name="Data Preparation Pipeline",
+        description="Data Preparation PipelineDescription",
+        continue_on_step_failure=True)
+    print("done!")
+
+    print("\nSTEP 9")
+    print("Submitting pipeline...")
+    experiment = Experiment(ws, 'Data_Preparation_Pipeline')
+    pipeline_run = experiment.submit(published_pipeline, regenerate_outputs=False)
--- bikeavailability/aml/ingestion/ingest.py
+++ bikeavailability/aml/ingestion/ingest.py
@@ -4,35 +4,39 @@ import os
 
 from util import print_nicely
 
-print_nicely("In ingest.py")
-print_nicely("As a data scientist, this is where I use my training code.")
+print_nicely("Inside ingest.py file")
 
-parser = argparse.ArgumentParser("train")
-
-parser.add_argument("--input_data", type=str, help="input data")
-parser.add_argument("--output_ingest", type=str, help="output_ingest directory")
-
+parser = argparse.ArgumentParser("ingest")
+parser.add_argument("--input_data", type=str, help="Location of the raw input data")
+parser.add_argument("--output_ingest", type=str, help="Output directory of the pipeline step")
+parser.add_argument("--converted_data_location", type=str, help="Location of the csv data (after json file conversion)")
 args = parser.parse_args()
 
 print_nicely("Argument 1: %s" % args.input_data)
 print_nicely("Argument 2: %s" % args.output_ingest)
-
-if not (args.output_ingest is None):
-    os.makedirs(args.output_ingest, exist_ok=True)
-    print_nicely("%s created" % args.output_ingest)
+print_nicely("Argument 3: %s" % args.converted_data_location)
 
 
 input_data = args.input_data
 
-processed_files = []
-
 print("Check what files are available in the source location..")
+processed_files = []
 for file in glob.glob(os.path.join(input_data, '*.*')):
     print(file)
     processed_files.extend([os.path.basename(file)])
 
+# if not (args.output_ingest is None):
+#     os.makedirs(args.output_ingest, exist_ok=True)
+#     print_nicely("%s created" % args.output_ingest)
 
-# write to output location
-with open(os.path.join(args.output_ingest, 'processed_files.txt'), 'w') as f:
+# # write to pipeline output location
+# with open(os.path.join(args.output_ingest, 'processed_files.csv'), 'w') as f:
+#     for item in processed_files:
+#         f.write(f"{item}\n")
+
+intermediate_data_path = os.path.join(args.converted_data_location, 'intermediate')
+os.makedirs(intermediate_data_path, exist_ok=True)
+
+with open(os.path.join(intermediate_data_path, 'processed_files.csv'), 'w') as f:
     for item in processed_files:
         f.write(f"{item}\n")
